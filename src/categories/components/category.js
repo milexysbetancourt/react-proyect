f00ec1259f1950b802ecc480bb320f37801935a1
Milexys Betancourt <milexysbetancourt1@gmail.com>
2018-12-14 11:23:48 -0300

    Layout con listado de playlist

---
+++ src/categories/components/category.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import Playlist from '../../playlist/components/playlist';
+//import "./playlist.css";
+
+function Category(props) {
+    return (
+        <div>
+            <p>{props.description}</p>
+            <h2>{props.title}</h2>
+            <Playlist 
+                playlist={props.playlist} 
+            />
+        </div>
+    )
+}
+
+export default Category
