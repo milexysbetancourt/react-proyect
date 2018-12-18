8ed86e2a0a9830464d1a1fb419612b1c01666b38
Milexys Betancourt <milexysbetancourt1@gmail.com>
2018-12-18 11:43:04 -0300

    Added title component

--- src/player/container/video-player.js
+++ src/player/container/video-player.js
@@ -1,13 +1,16 @@
 import React, { Component} from 'react';
 import VideoPlayerLayout from '../components/video-player'
+import Video from '../components/video';
+import Title from '../components/title';
+
 class VideoPlayer extends Component {
     render() {
         return (
             <VideoPlayerLayout>
-                <video 
-                    controls
-                    autoPlay
-                    src="http://download.blender.org/peach/bigbuckbunny_movies/BigBuckBunny_320x180.mp4"
+                <Title title="Mi título"/>
+                <Video 
+                    autoplay={false}
+                    src={"http://download.blender.org/peach/bigbuckbunny_movies/BigBuckBunny_320x180.mp4"}
                 />
             </VideoPlayerLayout>
         )
